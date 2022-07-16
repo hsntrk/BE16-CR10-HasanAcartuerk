b9710f776f790d311fc29025f4d910f6353307f7
hsntrk <hsk@mailbox.org>
2022-07-16 15:21:38 +0200

    finish

--- publisherd.php
+++ publisherd.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require_once "actions/db_connect.php";
 $publisher_name = $_GET["publisher_name"];
 $sql = "SELECT * FROM books WHERE publisher_name = '$publisher_name'";
